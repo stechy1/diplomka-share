92b809ebd84c75c60639303e529628c727c22f66
petr <stechy12@gmail.com>
2019-10-23 19:13:55 +0200

    Vytvoření sdíleného rozhraní definující odpověď ze serveru a expriment.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
+export interface ResponseObject<T> {
+    records: T;
+}
+
+export interface Experiment {
+    id: string;
+    name: string;
+    description: string;
+    type: string;
+    created: number;
+    output: {
+        led?: boolean;
+        image?: boolean;
+        sound?: boolean;
+    }
+}
+
 export const Greeter = (name: string) => `Hello ${name}`;
