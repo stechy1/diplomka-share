8de3f08362c50da17b4391f40c8d12b4385ac05a
petr <stechy12@gmail.com>
2020-01-25 17:32:26 +0100

    Upravení příkazů.

--- src/commands.ts
+++ src/commands.ts
@@ -2,7 +2,7 @@ const COMMAND_DELIMITER_SHARE = 0x53;
 
 const COMMAND_EXPERIMENT_STOP = 0x00;
 const COMMAND_EXPERIMENT_RUN = 0x01;
-const COMMAND_EXPERIMENT_INIT = 0x02;
+const COMMAND_EXPERIMENT_SETUP = 0x02;
 const COMMAND_EXPERIMENT_CLEAR = 0x03;
 const COMMAND_EXPERIMENT_READY = 0x05;
 
@@ -10,12 +10,6 @@ export class CommandToStimulator {
 
 	// Oddělovač příkazu
 	public static readonly COMMAND_DELIMITER = COMMAND_DELIMITER_SHARE;
-
-	// Příkaz pro reboot stimulátoru
-	public static readonly COMMAND_REBOOT = 0x00;
-
-	// Příkaz pro nastavení času
-	public static readonly COMMAND_TIME_SET = 0x01;
 
 	// Příkaz pro nastavení displaye
 	public static readonly COMMAND_DISPLAY = 0x02;
@@ -27,12 +21,11 @@ export class CommandToStimulator {
 	// Jednotlivé akce u správy experimentu
 	public static readonly COMMAND_MANAGE_EXPERIMENT_STOP = COMMAND_EXPERIMENT_STOP;
 	public static readonly COMMAND_MANAGE_EXPERIMENT_RUN = COMMAND_EXPERIMENT_RUN;
-	public static readonly COMMAND_MANAGE_EXPERIMENT_INIT = COMMAND_EXPERIMENT_INIT;
+	public static readonly COMMAND_MANAGE_EXPERIMENT_SETUP = COMMAND_EXPERIMENT_SETUP;
 	public static readonly COMMAND_MANAGE_EXPERIMENT_CLEAR = COMMAND_EXPERIMENT_CLEAR;
 	public static readonly COMMAND_MANAGE_EXPERIMENT_READY = COMMAND_EXPERIMENT_READY;
 
 
-	public static readonly COMMAND_EXPERIMENT_SETUP = 0x10;
 	public static readonly COMMAND_OUTPUT_SETUP = 0x11;
 
 
@@ -48,7 +41,7 @@ export class CommandFromStimulator {
 	public static readonly COMMAND_STIMULATOR_STATE = 0x01;
 	public static readonly COMMAND_STIMULATOR_STATE_STOP = COMMAND_EXPERIMENT_STOP;
 	public static readonly COMMAND_STIMULATOR_STATE_RUN = COMMAND_EXPERIMENT_RUN;
-	public static readonly COMMAND_STIMULATOR_STATE_INIT = COMMAND_EXPERIMENT_INIT;
+	public static readonly COMMAND_STIMULATOR_STATE_INIT = COMMAND_EXPERIMENT_SETUP;
 	public static readonly COMMAND_STIMULATOR_STATE_CLEAR = COMMAND_EXPERIMENT_CLEAR;
 	public static readonly COMMAND_STIMULATOR_STATE_READY = COMMAND_EXPERIMENT_READY;
 
