d9b66389229fb4794e81632eee2b2f043cf59082
petr <stechy12@gmail.com>
2020-09-14 17:55:41 +0200

    Změna rozhraní za třídu.

--- src/communication/client-server/client-socket-message.ts
+++ src/communication/client-server/client-socket-message.ts
@@ -2,8 +2,8 @@ import { SocketMessageType } from './socket-message-type';
 import { SocketMessageSpecialization } from './socket-message-specialization';
 import { SocketMessage } from './socket-message';
 
-export interface ClientReadyMessage extends SocketMessage {
-    type: SocketMessageType.CLIENT_READY;
-    specialization: SocketMessageSpecialization.CLIENT;
-    data: void;
+export class ClientReadyMessage implements SocketMessage {
+    public readonly type = SocketMessageType.CLIENT_READY;
+    public readonly specialization = SocketMessageSpecialization.CLIENT;
+    public readonly data = null;
 }
